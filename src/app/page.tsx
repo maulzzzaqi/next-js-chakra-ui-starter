ef78fd8c47659e6d3c59ddec6ac48a65c940ee29
MaulzzZaqi <madezaqi2006@gmail.com>
2025-10-07 18:23:35 +0700

    dummy chatbot ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,24 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button, Card, CardBody, Container, Flex, HStack, Input, Text } from "@chakra-ui/react";
+import { AddIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Flex direction="column" minH="100vh" justify="space-between">
+      <Flex direction="column" align="center" justify="center" flex="1">
+        <Text fontSize="3xl" fontWeight="semibold">Good Morning!</Text>
+      </Flex>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Card bg="blackAlpha.200" shadow="xl" border="1px" borderRadius="50" borderColor="gray.200" mx="6" mb="20">
+        <CardBody display="flex" color="gray" alignItems="center" py="4" px="6" flex="1">
+          <Button colorScheme="blackAlpha" variant="ghost" borderRadius="100"><AddIcon /></Button>
+          <Input bg="white" border="none" focusBorderColor="transparent" placeholder="Ask your question here..." mx="4" py="6" />
+          <Button colorScheme="blackAlpha" variant="solid"><ArrowForwardIcon /></Button>
+        </CardBody>
+      </Card>
+    </Flex>
   );
 }
